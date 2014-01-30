093e3d958d6fce62aff1da95e4612296d969ab27
teabait <hello.teaho@gmail.com>
2014-01-29 20:42:59 -0500

    tests passing

--- lorem.js
+++ lorem.js
@@ -1,26 +1,87 @@
+//   __ _ _ __  _ __
+//  / _` | '_ \| '_ \
+// | (_| | |_) | |_) |
+//  \__,_| .__/| .__/
+//       |_|   |_|
+
 var LoremIpsumGen = function(listOfWords, num) {
   this.listOfWords = listOfWords.split(" ");
   this.num = num;
+  this.wordBucket = [];
 }
 
+//                 _
+//  ___  ___ _ __ | |_ ___ _ __   ___ ___
+// / __|/ _ \ '_ \| __/ _ \ '_ \ / __/ _ \
+// \__ \  __/ | | | ||  __/ | | | (_|  __/
+// |___/\___|_| |_|\__\___|_| |_|\___\___|
+
 var Sentence = function(words) {
-  this.toString = words.join(" ");
+  this.words = words;
 }
+//                                              _
+//  _ __   __ _ _ __ __ _  __ _ _ __ __ _ _ __ | |__
+// | '_ \ / _` | '__/ _` |/ _` | '__/ _` | '_ \| '_ \
+// | |_) | (_| | | | (_| | (_| | | | (_| | |_) | | | |
+// | .__/ \__,_|_|  \__,_|\__, |_|  \__,_| .__/|_| |_|
+// |_|                    |___/          |_|
 
 var Paragraph = function() {
   this.sentences = [];
-  this.toString = this.sentences.join(" ");
 }
 
-LoremIpsumGen.prototype.createSentence = function(){
+//                        __                  _   _
+//   __ _ _ __  _ __     / _|_   _ _ __   ___| |_(_) ___  _ __  ___
+//  / _` | '_ \| '_ \   | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+// | (_| | |_) | |_) |  |  _| |_| | | | | (__| |_| | (_) | | | \__ \
+//  \__,_| .__/| .__/   |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+//       |_|   |_|
+
+// creates a blob of n words, n being the number of words user asks for
+LoremIpsumGen.prototype.fillBucket = function() {
+  for(var i = 0; i < this.num; i++) {
+    this.wordBucket.push(this.listOfWords[Math.floor(Math.random()*this.listOfWords.length)]);
+  }
+  return this.wordBucket;
+}
+
+// creates sentence made of words from this.wordBucket
+// each sentence is 4-12 words long
+// returns sentence as a string
+LoremIpsumGen.prototype.createSentence = function () {
   var sentenceWords = [];
   // varied sentence length
-  for(var i = 0; i <= Math.floor(Math.random() * 8) + 4; i++) {
-    // get random word from input list
-    sentenceWords.push(this.listOfWords[Math.floor(Math.random()*this.listOfWords.length)]);
-    var sentence = new Sentence(sentenceWords);
-    return sentence.toString;
+  var numWords = Math.floor(Math.random() * 8) + 4;
+  for(var i = 0; i <= numWords; i++) {
+    // get random word from input wordBucket
+    var firstWord = this.wordBucket.shift();
+    sentenceWords.push(firstWord);
   }
+  var sentence = new Sentence(sentenceWords);
+  return sentence.toString();
+}
+
+LoremIpsumGen.prototype.createSentences = function () {
+  var sentences = [];
+
+  while (this.wordBucket.length > 0) {
+    this.createSentence();
+  }
+
+  return sentences.hej('tea');
+};
+
+// make sentence presentable:
+// array to string
+// capitalize first letter
+// add punctuation at end
+Sentence.prototype.toString = function () {
+  return this.words.join(" ");
+}
+
+// turns array of sentences into a cohesive paragraph
+Paragraph.prototype.toString = function () {
+  this.sentences.join(" ");
 }
 
 module.exports = LoremIpsumGen;
--- lorem_test.spec.js
+++ lorem_test.spec.js
@@ -1,13 +1,19 @@
 var Lorem = require('./lorem');
 
 describe("Lorem", function() {
-  var lorem = new Lorem("Harry Potter Hermione Ron", 5);
 
   it("requires user input", function() {
     expect(function() { new Lorem();} ).toThrow("Cannot call method 'split' of undefined");
   });
 
+  it("fills word bucket up to user input", function() {
+    var lorem = new Lorem("Harry Potter Hermione Ron", 10);
+    lorem.fillBucket();
+    expect(lorem.wordBucket.length).toEqual(10);
+  });
+
   it("returns a sentence as a string", function() {
+    var lorem = new Lorem("Harry Potter Hermione Ron", 10);
     var result = lorem.createSentence();
     expect(typeof result).toEqual("string");
   });
